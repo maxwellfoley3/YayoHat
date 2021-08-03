52fb13d6ab89832edfab1f3407d01beec64748ad
Maxwell Foley <maxwell.foley@certik.org>
2021-08-03 19:02:40 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# YayoHat
+
+For use in development, download [Ganache](https://www.trufflesuite.com/ganache) to run a local blockchain instance, which Metamask can then point towards. Then run `truffle migrate` to launch the various contracts and recieve their addresses in the console. 
--- migrations/2_deploy_yayo_hat.js
+++ migrations/2_deploy_yayo_hat.js
@@ -12,5 +12,4 @@ module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(HatNFT, HatToken.address, NUMBER_OF_HATS);
   await deployer.deploy(MockYayoToken, 10000000)
   await deployer.deploy(YayoSwapRouter, YayoSwap.address, MockYayoToken.address, HatToken.address)
-
 };
